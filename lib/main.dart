b0972bcbdfd29a4b7c8a80d7ac8bc4245bf89927
Muhammad-ArifinDev <ipin93936@gmail.com>
2025-10-27 22:18:11 +0700

    menambahkan Latihan 2 bottomnavigation 3 tab

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,52 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Latihan1App());
+  runApp(const Latihan2App());
 }
 
-class Latihan1App extends StatelessWidget {
-  const Latihan1App({super.key});
+class Latihan2App extends StatefulWidget {
+  const Latihan2App({super.key});
+
+  @override
+  State<Latihan2App> createState() => _Latihan2AppState();
+}
+
+class _Latihan2AppState extends State<Latihan2App> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _pages = <Widget>[
+    Center(child: Text("Halaman Home", style: TextStyle(fontSize: 22))),
+    Center(child: Text("Halaman Profil", style: TextStyle(fontSize: 22))),
+    Center(child: Text("Halaman Pengaturan", style: TextStyle(fontSize: 22))),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Latihan 1',
+      title: 'Latihan 2',
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Latihan 1 - AppBar dan Body"),
+          title: const Text("Latihan 2 - BottomNavigationBar"),
           backgroundColor: Colors.blueAccent,
         ),
-        body: const Center(
-          child: Text(
-            "Muhammad Arifin",
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
+        body: _pages[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.blue,
+          onTap: _onItemTapped,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profil"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), label: "Pengaturan"),
+          ],
         ),
       ),
     );
